ad5c5d2990f4c892673d4ec081d68a4569b347e6
Alexander Shelepenok <alxshelepenok@gmail.com>
2022-11-17 17:53:29 +0000

    fix(tests): remove gatsby-core-utils from moduleNameMapper

--- internal/testing/jest-config.ts
+++ internal/testing/jest-config.ts
@@ -20,7 +20,6 @@ const jestConfig: Config.InitialOptions = {
     ".+\\.(css|sass|scss)$": "identity-obj-proxy",
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "identity-obj-proxy",
-    "^gatsby-core-utils/(.*)$": "gatsby-core-utils/dist/$1",
     "^gatsby-plugin-utils/(.*)$": [
       "gatsby-plugin-utils/dist/$1",
       "gatsby-plugin-utils/$1",
